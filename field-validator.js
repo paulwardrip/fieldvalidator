8ac19072f5e2edccff9441ed2dfb58cab4201448
Paul Wardrip <pwardrip@gmail.com>
2016-10-12 16:00:45 -0400

    Fix to titlecase

--- field-validator.js
+++ field-validator.js
@@ -34,7 +34,7 @@ String.prototype.pad = function (max, chr) {
 };
 
 String.prototype.toTitleCase = function () {
-    return this.replace(/\b\w+/g, function(txt){
+    return this.replace(/\b(\w|')+/g, function(txt){
         if ($.inArray(txt.toLowerCase(), ["of", "and", "for"]) > -1) { return txt.toLowerCase();
         } else { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         }});
